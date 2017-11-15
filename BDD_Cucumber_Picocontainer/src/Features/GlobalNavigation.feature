0ea575b5dcde963f7ce8c16c7075e0f650535e18
Murp01 <pmurphy35@hotmail.com>
2017-11-15 21:12:02 +0000

    BDD Framework Picocontainer
    My first Java cucumber framework using picocontainer

---
+++ BDD_Cucumber_Picocontainer/src/Features/GlobalNavigation.feature
@@ -0,0 +1,8 @@
+Feature: Global Navigation - Global nav displayed globally across all pages
+
+Scenario: Click on the About Us title
+
+Given the user is on the homepage
+When the user clicks on the AboutUs title
+Then confirm the webpages title
+
---
+++ BDD_Cucumber_Picocontainer/src/stepDefinitions/SD_HomePage.java
@@ -0,0 +1,33 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class SD_HomePage {
+	public WebDriver driver;
+	
+	@Given("^the user is on the homepage$")
+	public void WebDriverInitializer() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Eclipse\\Webdrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://linklaters.com");
+	}
+	
+	
+	@When("^the user clicks on the AboutUs title$")
+	public void the_user_clicks_on_the_AboutUs_title() throws Throwable {
+		driver.findElement(By.xpath("//a[@href='/en/about-us']")).click();
+	}
+
+	@Then("^confirm the webpages title$")
+	public void confirm_the_webpages_title() throws Throwable {
+
+	}
+
+}
